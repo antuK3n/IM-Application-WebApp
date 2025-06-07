388985c30bd29c6c45d1b13f8ab678972cc79343
Ken Mondragon <kenjohnvianneym@gmail.com>
2025-06-07 12:17:54 +0800

    fix

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-        serverComponentsExternalPackages: ["pdfkit"],
-    }
+    serverExternalPackages: ["pdfkit"]
 };
 
 export default nextConfig;
